b7e35f2884194bbd760dcfe006261c1550625343
ErwinKomen <E.Komen@ru.nl>
2019-01-21 12:20:49 +0100

    Project site

---
+++ passimproject/js/passim.project.js
@@ -0,0 +1,63 @@
+﻿var django = {
+  "jQuery": jQuery.noConflict(true)
+};
+var jQuery = django.jQuery;
+var $ = jQuery;
+
+(function ($) {
+  $(function () {
+    $(document).ready(function () {
+      // Initialize event listeners
+      ru.passim.project.init_event_listeners();
+      // Initialize Bootstrap popover
+      // Note: this is used when hovering over the question mark button
+      $('[data-toggle="popover"]').popover();
+    });
+  });
+})(django.jQuery);
+
+
+// based on the type, action will be loaded
+
+// var $ = django.jQuery.noConflict();
+
+var ru = (function ($, ru) {
+  "use strict";
+
+  ru.passim.project = (function ($, config) {
+    // Define variables for ru.collbank here
+    var loc_divErr = "passim_err",
+        base_url = "",
+        oSyncTimer = null;
+
+    // Private methods specification
+    var private_methods = {
+      /**
+       * methodNotVisibleFromOutside - example of a private method
+       * @returns {String}
+       */
+      methodNotVisibleFromOutside: function () {
+        return "something";
+      },
+      errClear: function() {
+        $("#" + loc_divErr).html("");
+      },
+      errMsg: function (sMsg, ex) {
+        var sHtml = "Error in [" + sMsg + "]<br>" + ex.message;
+        $("#" + loc_divErr).html(sHtml);
+      }
+    }
+
+    // Public methods
+    return {
+
+      init_event_listeners : function() {
+
+      }
+
+    };
+  }($, ru.config));
+
+  return ru;
+}(jQuery, window.ru || {})); // window.ru: see http://stackoverflow.com/questions/21507964/jslint-out-of-scope
+
