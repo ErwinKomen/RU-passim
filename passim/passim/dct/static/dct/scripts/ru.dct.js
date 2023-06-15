524ed7c3e3b87606e043b2c53429e8a0a3ebf719
ErwinKomen <E.Komen@ru.nl>
2023-06-15 08:39:35 +0200

    issue #574: small follow-up

--- passim/passim/dct/static/dct/scripts/ru.dct.js
+++ passim/passim/dct/static/dct/scripts/ru.dct.js
@@ -2242,67 +2242,71 @@ var ru = (function ($, ru) {
           // Get the table
           table = $(elStart).closest(".related-original").find("table.func-view.related.sel-table").first();
 
-          // Double check
-          $.post(targeturl, data, function (response) {
-            // Action depends on the response
-            if (response === undefined || response === null || !("status" in response)) {
-              private_methods.errMsg("No status returned");
-            } else {
-              switch (response.status) {
-                case "ready":
-                case "ok":
-                  // Should have a new target URL
-                  targeturl = response['targeturl'];
-                  action = response['action'];
-                  if (targeturl !== undefined && targeturl !== "") {
-                    // Go open that targeturl
-                    window.location = targeturl;
-                  } else if (action !== undefined && action !== "") {
-                    switch (action) {
-                      case "added":
-                        // Hide the interface
-                        $("#sgroup-add").addClass("collapse");
-                        $("#sgroup-add").removeClass("in");
-                        // Make sure that the savegroup name row is added to the table appropriately
-                        sGroupNew = response['sgroupnew'];
-                        // Add this to the table
-                        $(table).find("tbody:last-child").append(sGroupNew);
-                        break;
-                    }
-                  }
-                  break;
-                case "error":
-                  if ("html" in response) {
-                    // Show the HTML in the targetid
-                    $(err).html(response['html']);
-                    // If there is an error, indicate this
-                    if (response.status === "error") {
-                      if ("msg" in response) {
-                        if (typeof response['msg'] === "object") {
-                          lHtml = []
-                          lHtml.push("Errors:");
-                          $.each(response['msg'], function (key, value) { lHtml.push(key + ": " + value); });
-                          $(err).html(lHtml.join("<br />"));
-                        } else {
-                          $(err).html("Error: " + response['msg']);
-                        }
-                      } else {
-                        $(err).html("<code>There is an error</code>");
+          if (sAction === "add_sgroup") {
+
+            // Double check
+            $.post(targeturl, data, function (response) {
+              // Action depends on the response
+              if (response === undefined || response === null || !("status" in response)) {
+                private_methods.errMsg("No status returned");
+              } else {
+                switch (response.status) {
+                  case "ready":
+                  case "ok":
+                    // Should have a new target URL
+                    targeturl = response['targeturl'];
+                    action = response['action'];
+                    if (targeturl !== undefined && targeturl !== "") {
+                      // Go open that targeturl
+                      window.location = targeturl;
+                    } else if (action !== undefined && action !== "") {
+                      switch (action) {
+                        case "added":
+                          // Hide the interface
+                          $("#sgroup-add").addClass("collapse");
+                          $("#sgroup-add").removeClass("in");
+                          // Make sure that the savegroup name row is added to the table appropriately
+                          sGroupNew = response['sgroupnew'];
+                          // Add this to the table
+                          $(table).find("tbody:last-child").append(sGroupNew);
+                          break;
                       }
                     }
-                  } else {
-                    // Send a message
-                    $(err).html("<i>There is no <code>html</code> in the response from the server</i>");
-                  }
-                  break;
-                default:
-                  // Something went wrong -- show the page or not?
-                  $(err).html("The status returned is unknown: " + response.status);
-                  break;
-              }
+                    break;
+                  case "error":
+                    if ("html" in response) {
+                      // Show the HTML in the targetid
+                      $(err).html(response['html']);
+                      // If there is an error, indicate this
+                      if (response.status === "error") {
+                        if ("msg" in response) {
+                          if (typeof response['msg'] === "object") {
+                            lHtml = []
+                            lHtml.push("Errors:");
+                            $.each(response['msg'], function (key, value) { lHtml.push(key + ": " + value); });
+                            $(err).html(lHtml.join("<br />"));
+                          } else {
+                            $(err).html("Error: " + response['msg']);
+                          }
+                        } else {
+                          $(err).html("<code>There is an error</code>");
+                        }
+                      }
+                    } else {
+                      // Send a message
+                      $(err).html("<i>There is no <code>html</code> in the response from the server</i>");
+                    }
+                    break;
+                  default:
+                    // Something went wrong -- show the page or not?
+                    $(err).html("The status returned is unknown: " + response.status);
+                    break;
+                }
 
-            }
-          });
+              }
+            });
+
+          }
 
 
 
